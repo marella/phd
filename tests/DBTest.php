4b1a74c4c031ba0082d3dfe63266688d1716516b
Ravindra Marella <mv.ravindra007@gmail.com>
2016-02-22 03:56:44 +0530

    [tests] Add more tests for DB callStatic method

--- tests/DBTest.php
+++ tests/DBTest.php
@@ -65,12 +65,31 @@ class DBTest extends PHPUnit_Framework_TestCase
     {
         $this->assertNull(DB::getFacadeRoot());
 
-        $db = $this->getMock('DatabaseManagerStub', ['foo', 'bar']);
-        $db->expects($this->once())->method('foo')->with('a')->will($this->returnValue('b'));
-        $db->expects($this->once())->method('bar')->will($this->returnValue('c'));
+        $db = $this->getMock('DatabaseManagerStub', ['a', 'b', 'c', 'd', 'e', 'f', 'g']);
+        $db->expects($this->once())->method('a')->will($this->returnValue('ar'));
+        $db->expects($this->once())->method('b')->with('b1')->will($this->returnValue('br'));
+        $db->expects($this->once())->method('c')->with('c1', 'c2')->will($this->returnValue('cr'));
+        $db->expects($this->once())->method('d')->with('d1', 'd2', 'd3')->will($this->returnValue('dr'));
+        $db->expects($this->once())->method('e')->with('e1', 'e2', 'e3', 'e4')->will($this->returnValue('er'));
+        $db->expects($this->once())->method('f')->with('f1', 'f2', 'f3', 'f4', 'f5')->will($this->returnValue('fr'));
+        $db->expects($this->once())->method('g')->with('g1', 'g2', 'g3', 'g4', 'g5', 'g6')->will($this->returnValue('gr'));
+
         DB::setFacadeRoot($db);
-        $this->assertEquals('b', DB::foo('a'));
-        $this->assertEquals('c', DB::bar());
+
+        $this->assertEquals('ar', DB::a());
+        $this->assertEquals('br', DB::b('b1'));
+        $this->assertEquals('cr', DB::c('c1', 'c2'));
+        $this->assertEquals('dr', DB::d('d1', 'd2', 'd3'));
+        $this->assertEquals('er', DB::e('e1', 'e2', 'e3', 'e4'));
+        $this->assertEquals('fr', DB::f('f1', 'f2', 'f3', 'f4', 'f5'));
+        $this->assertEquals('gr', DB::g('g1', 'g2', 'g3', 'g4', 'g5', 'g6'));
+    }
+
+    public function testCallStaticThrowsException()
+    {
+        $this->assertNull(DB::getFacadeRoot());
+        $this->setExpectedException('RuntimeException');
+        DB::foo();
     }
 }
 
