8183c770cb35662d29071d87fdd8f86013347eba
Ravindra Marella <mv.ravindra007@gmail.com>
2016-02-22 04:04:57 +0530

    [tests] Add tests for Support\Arr

---
+++ tests/SupportArrTest.php
@@ -0,0 +1,91 @@
+<?php
+
+use PhD\Support\Arr;
+
+class SupportArrTest extends PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+        $array = Arr::add(['name' => 'Desk'], 'price', 100);
+        $this->assertEquals(['name' => 'Desk', 'price' => 100], $array);
+    }
+
+    public function testSet()
+    {
+        $array = ['products' => ['desk' => ['price' => 100]]];
+        Arr::set($array, 'products.desk.price', 200);
+        $this->assertEquals(['products' => ['desk' => ['price' => 200]]], $array);
+    }
+
+    public function testExcept()
+    {
+        $array = ['name' => 'Desk', 'price' => 100];
+        $array = Arr::except($array, ['price']);
+        $this->assertEquals(['name' => 'Desk'], $array);
+    }
+
+    public function testGet()
+    {
+        $array = ['products' => ['desk' => ['price' => 100]]];
+        $value = Arr::get($array, 'products.desk');
+        $this->assertEquals(['price' => 100], $value);
+        // Test direct ArrayAccess object
+        $array = ['products' => ['desk' => ['price' => 100]]];
+        $arrayAccessObject = new ArrayObject($array);
+        $value = Arr::get($arrayAccessObject, 'products.desk');
+        $this->assertEquals(['price' => 100], $value);
+        // Test array containing ArrayAccess object
+        $arrayAccessChild = new ArrayObject(['products' => ['desk' => ['price' => 100]]]);
+        $array = ['child' => $arrayAccessChild];
+        $value = Arr::get($array, 'child.products.desk');
+        $this->assertEquals(['price' => 100], $value);
+        // Test array containing multiple nested ArrayAccess objects
+        $arrayAccessChild = new ArrayObject(['products' => ['desk' => ['price' => 100]]]);
+        $arrayAccessParent = new ArrayObject(['child' => $arrayAccessChild]);
+        $array = ['parent' => $arrayAccessParent];
+        $value = Arr::get($array, 'parent.child.products.desk');
+        $this->assertEquals(['price' => 100], $value);
+        // Test missing ArrayAccess object field
+        $arrayAccessChild = new ArrayObject(['products' => ['desk' => ['price' => 100]]]);
+        $arrayAccessParent = new ArrayObject(['child' => $arrayAccessChild]);
+        $array = ['parent' => $arrayAccessParent];
+        $value = Arr::get($array, 'parent.child.desk');
+        $this->assertNull($value);
+        // Test null ArrayAccess object field
+        $arrayAccessObject = new ArrayObject(['products' => ['desk' => null]]);
+        $array = ['parent' => $arrayAccessObject];
+        $value = Arr::get($array, 'parent.products.desk.price');
+        $this->assertNull($value);
+    }
+
+    public function testForget()
+    {
+        $array = ['products' => ['desk' => ['price' => 100]]];
+        Arr::forget($array, null);
+        $this->assertEquals(['products' => ['desk' => ['price' => 100]]], $array);
+        $array = ['products' => ['desk' => ['price' => 100]]];
+        Arr::forget($array, []);
+        $this->assertEquals(['products' => ['desk' => ['price' => 100]]], $array);
+        $array = ['products' => ['desk' => ['price' => 100]]];
+        Arr::forget($array, 'products.desk');
+        $this->assertEquals(['products' => []], $array);
+        $array = ['products' => ['desk' => ['price' => 100]]];
+        Arr::forget($array, 'products.desk.price');
+        $this->assertEquals(['products' => ['desk' => []]], $array);
+        $array = ['products' => ['desk' => ['price' => 100]]];
+        Arr::forget($array, 'products.final.price');
+        $this->assertEquals(['products' => ['desk' => ['price' => 100]]], $array);
+        $array = ['shop' => ['cart' => [150 => 0]]];
+        Arr::forget($array, 'shop.final.cart');
+        $this->assertEquals(['shop' => ['cart' => [150 => 0]]], $array);
+        $array = ['products' => ['desk' => ['price' => ['original' => 50, 'taxes' => 60]]]];
+        Arr::forget($array, 'products.desk.price.taxes');
+        $this->assertEquals(['products' => ['desk' => ['price' => ['original' => 50]]]], $array);
+        $array = ['products' => ['desk' => ['price' => ['original' => 50, 'taxes' => 60]]]];
+        Arr::forget($array, 'products.desk.final.taxes');
+        $this->assertEquals(['products' => ['desk' => ['price' => ['original' => 50, 'taxes' => 60]]]], $array);
+        $array = ['products' => ['desk' => ['price' => 50], null => 'something']];
+        Arr::forget($array, ['products.amount.all', 'products.desk.price']);
+        $this->assertEquals(['products' => ['desk' => [], null => 'something']], $array);
+    }
+}
