846a037db3aedb616ea14f8720dbfccc5e983418
Ravindra Marella <mv.ravindra007@gmail.com>
2016-02-22 04:12:43 +0530

    [tests] Add tests for Support\Str

---
+++ tests/SupportStrTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use PhD\Support\Str;
+
+class SupportStrTest extends PHPUnit_Framework_TestCase
+{
+    public function testStrContains()
+    {
+        $this->assertTrue(Str::contains('taylor', 'ylo'));
+        $this->assertTrue(Str::contains('taylor', 'taylor'));
+        $this->assertTrue(Str::contains('taylor', ['ylo']));
+        $this->assertTrue(Str::contains('taylor', ['xxx', 'ylo']));
+        $this->assertFalse(Str::contains('taylor', 'xxx'));
+        $this->assertFalse(Str::contains('taylor', ['xxx']));
+        $this->assertFalse(Str::contains('taylor', ''));
+    }
+
+    public function testEndsWith()
+    {
+        $this->assertTrue(Str::endsWith('jason', 'on'));
+        $this->assertTrue(Str::endsWith('jason', 'jason'));
+        $this->assertTrue(Str::endsWith('jason', ['on']));
+        $this->assertTrue(Str::endsWith('jason', ['no', 'on']));
+        $this->assertFalse(Str::endsWith('jason', 'no'));
+        $this->assertFalse(Str::endsWith('jason', ['no']));
+        $this->assertFalse(Str::endsWith('jason', ''));
+        $this->assertFalse(Str::endsWith('7', ' 7'));
+    }
+}
